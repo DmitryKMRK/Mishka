f5b458cb43863cf5f6dce7539dfa1dfcf999613a
DmitryKMRK <dmitry.kmrk@gmail.com>
2017-08-09 13:29:37 +0300

    Adaptive styles

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var toggle = document.querySelector('.header__toggle');
+var userBlock = document.querySelector('.header__user-block');
+var nav = document.querySelector('.header__nav');
+var state = 'close';
+
+nav.classList.remove('header__nav--open');
+toggle.classList.remove('header__toggle--nojs');
+userBlock.classList.remove('header__user-block--open');
+
+toggle.addEventListener('click', function() {
+  if(state === 'close') {
+    nav.classList.add('header__nav--open');
+    userBlock.classList.add('header__user-block--open');
+    toggle.classList.add('header__toggle--open');
+    state = 'open';
+  } else {
+    nav.classList.remove('header__nav--open');
+    userBlock.classList.remove('header__user-block--open');
+    toggle.classList.remove('header__toggle--open');
+    state = 'close';
+  }
+});
